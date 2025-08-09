433315a5e07d5abbfb7e78084acbc3166533f93c
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-09 18:57:37 -0400

    Add Lesson_5_Design_Patterns_in_System_Architecture/part_1_singleton.py

---
+++ Lesson_5_Design_Patterns_in_System_Architecture/part_1_singleton.py
@@ -0,0 +1,10 @@
+class Singleton:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Singleton, cls).__new__(cls)
+        return cls._instance
+
+    def some_business_logic(self):
+        return "Performing some business logic"
