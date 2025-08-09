948bb290f4be3bc7d2e3619a396f2fb935d98b36
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-09 18:57:35 -0400

    Add Lesson_4_Overview_of_Financial_Trading_Systems/part_7_monitoring_system.py

---
+++ Lesson_4_Overview_of_Financial_Trading_Systems/part_7_monitoring_system.py
@@ -0,0 +1,8 @@
+class MonitoringSystem:
+    def log_event(self, event):
+        # Code to log performance metrics
+        pass
+
+    def monitor_latency(self):
+        # Code to measure system's internal latency
+        pass
