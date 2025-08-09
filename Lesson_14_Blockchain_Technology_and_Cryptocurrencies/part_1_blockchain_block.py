c833f2cf6a6dd728a72e3018ce3d6041499d781e
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-09 18:56:54 -0400

    Add Lesson_14_Blockchain_Technology_and_Cryptocurrencies/part_1_blockchain_block.py

---
+++ Lesson_14_Blockchain_Technology_and_Cryptocurrencies/part_1_blockchain_block.py
@@ -0,0 +1,15 @@
+import hashlib
+import json
+from time import time
+
+class Block:
+    def __init__(self, index, previous_hash, transactions, timestamp=None):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.transactions = transactions
+        self.timestamp = timestamp or time()
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_string = json.dumps(self.__dict__, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
